63229b863e03e628036f94a179f9426ea028ccbf
Querela <ek.92@arcor.de>
2013-03-23 02:45:26 +0000

    Added FEN import code. Needs completion ...

--- datamodel/src/de/uni_leipzig/informatik/swp13_sc/datamodel/ChessBoard.java
+++ datamodel/src/de/uni_leipzig/informatik/swp13_sc/datamodel/ChessBoard.java
@@ -10,6 +10,7 @@ import de.uni_leipzig.informatik.swp13_sc.datamodel.ChessFigureType;
 import de.uni_leipzig.informatik.swp13_sc.datamodel.ChessMove;
 import de.uni_leipzig.informatik.swp13_sc.datamodel.ChessPosition;
 
+import java.lang.IllegalArgumentException;
 import java.lang.StringBuilder;
 
 import java.util.ArrayList;
@@ -20,9 +21,11 @@ public class ChessBoard
     private ChessFigure[][] field = new ChessFigure[8][8];
     private List<ChessFigure> figuresLeft = new ArrayList<ChessFigure>();
     private List<ChessMove> moves = new ArrayList<ChessMove>();
+    private boolean canUseFigurePositions = false;
     
     public ChessBoard()
     {
+        this.canUseFigurePositions = true;
         for (ChessFigure f : ChessFigure.values())
         {
             ChessPosition pos = f.getStartPosition();
@@ -34,9 +37,112 @@ public class ChessBoard
     }
     
     public ChessBoard(String fen)
+        throws IllegalArgumentException
     {
         // TODO: create ChessBoard with FEN
         //       set & move Figures, create Moves
+        String position;
+        if (fen.indexOf(' ') == -1)
+        {
+            position = fen;
+        }
+        else
+        {
+            position = fen.substring(0, fen.indexOf(' ')); // TODO: check!
+        }
+        String[] parts = position.split("/");
+        if (parts.length != 8)
+        {
+            // ERROR
+            // TODO: what now?
+            throw new IllegalArgumentException("Not 8 Rows in FEN: " + position + ", orig: " + fen);
+        }
+        
+        List<ChessFigure> fig = new ArrayList<ChessFigure>();
+        for (ChessFigure f : ChessFigure.values())
+        {
+            fig.add(f);
+        } // addAll() ???
+        this.canUseFigurePositions = false;
+        for (int y = 7; y >= 0; y --)
+        {
+            String row = parts[y];
+            int x = 0;
+            for (char p : row.toCharArray())
+            {
+                if (x >= 8)
+                {
+                    throw new IllegalArgumentException("Too long row! - " + row + " in " + fen);
+                }
+                switch (p)
+                {
+                    case '8':
+                        // this.field[y][x] = null; // should be null
+                        x ++; 
+                    case '7':
+                        x ++;
+                    case '6':
+                        x ++;
+                    case '5':
+                        x ++;
+                    case '4':
+                        x ++;
+                    case '3':
+                        x ++;
+                    case '2':
+                        x ++;
+                    case '1':
+                        x ++;
+                        break;
+                    case 'P':
+                        // TODO: which Pawn ???
+                        //       Add some Pawns without Positioning?
+                        //       need some next fig function ...
+                        break;
+                    case 'p':
+                        break;
+                    case 'R':
+                        break;
+                    case 'r':
+                        break;
+                    case 'N':
+                        break;
+                    case 'n':
+                        break;
+                    case 'B':
+                        break;
+                    case 'b':
+                        break;
+                    case 'Q':
+                        if (fig.remove(ChessFigure.WhiteQueen))
+                        {
+                            this.field[y][x] = ChessFigure.WhiteQueen;
+                        }
+                        else
+                        {
+                            throw new IllegalArgumentException("Multiple Queens! - " + row + " in " + fen);
+                        }
+                        break;
+                    case 'q':
+                        if (fig.remove(ChessFigure.BlackQueen))
+                        {
+                            this.field[y][x] = ChessFigure.BlackQueen;
+                        }
+                        else
+                        {
+                            throw new IllegalArgumentException("Multiple black Queens! - " + row + " in " + fen);
+                        }
+                        break;
+                    case 'K':
+                        break;
+                    case 'k':
+                        break;
+                    default:
+                        // should not happen
+                        throw new IllegalArgumentException("Unknown Character: " + p + " in " + row + " in " + fen);
+                }
+            }
+        }
     }
     
     
