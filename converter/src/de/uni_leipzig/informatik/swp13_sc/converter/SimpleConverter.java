4759b1c2424ae6b168a81eea981fc3e34302bce6
Querela <ek.92@arcor.de>
2013-03-25 19:25:37 +0100

    An example converter ... output module is still missing (and input parsing)

--- converter/src/de/uni_leipzig/informatik/swp13_sc/converter/SimpleConverter.java
+++ converter/src/de/uni_leipzig/informatik/swp13_sc/converter/SimpleConverter.java
@@ -5,7 +5,11 @@
 package de.uni_leipzig.informatik.swp13_sc.converter;
 
 import de.uni_leipzig.informatik.swp13_sc.converter.Converter;
+import de.uni_leipzig.informatik.swp13_sc.converter.input.FileInput;
+import de.uni_leipzig.informatik.swp13_sc.converter.input.Input;
 import de.uni_leipzig.informatik.swp13_sc.converter.input.PGNFileInput;
+import de.uni_leipzig.informatik.swp13_sc.converter.output.FileOutput;
+import de.uni_leipzig.informatik.swp13_sc.converter.output.Output;
 
 /**
  *
@@ -29,24 +33,45 @@ public class SimpleConverter extends Converter
 		if (this.supportsInput(inputFormat))
 		{
 			this.inputArgs = inputArgs;
-			super.setInput(super.getInput(inputFormat));
+			Input i = super.getInput(inputFormat);
+			if (i instanceof FileInput)
+			{
+				FileInput fi = (FileInput) i;
+				fi.setFilename(this.inputArgs);
+			}
+			super.setInput(i);
 		}
 		if (this.supportsOutput(outputFormat))
 		{
 			this.outputArgs = outputArgs;
-			super.setOutput(super.getOutput(outputFormat));
+			Output o = super.getOutput(outputFormat);
+			if (o instanceof FileOutput)
+			{
+				FileOutput fo = (FileOutput) o;
+				// set data ... outputArgs
+			}
+			super.setOutput(o);
 		}
 		
 		// TODO: args parsing / module choosing ...
 	}
 
 	/**
-	 * 
+	 * Sample Module loading
 	 */
 	private void loadInputsAndOutputs()
 	{
 		super.addInput(new PGNFileInput(this));
 		// TODO: ...
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		SimpleConverter sc = new SimpleConverter("pgn", "filname.pgn", "ttl", "filename.ttl");
+		
+		sc.beginConvert();
 	}
 	
 	
