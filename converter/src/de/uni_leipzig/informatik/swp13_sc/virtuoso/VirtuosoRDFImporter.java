19c019bc91c0391b982fd3b01115ad76d53dfb74
Querela <ek.92@arcor.de>
2013-04-24 16:19:59 +0200

    basic implementation of importer - doesn't work ...
    Error while adding Triples:
    SR185: Undefined procedure DB.DBA.__rdf_graph_is_in_enabled_repl.
    -> http://docs.openlinksw.com/virtuoso/errors.html#errSR185

--- converter/src/de/uni_leipzig/informatik/swp13_sc/virtuoso/VirtuosoRDFImporter.java
+++ converter/src/de/uni_leipzig/informatik/swp13_sc/virtuoso/VirtuosoRDFImporter.java
@@ -5,15 +5,18 @@ package de.uni_leipzig.informatik.swp13_sc.virtuoso;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import virtuoso.jena.driver.VirtGraph;
 import virtuoso.jena.driver.VirtModel;
 
 import com.hp.hpl.jena.graph.Graph;
 import com.hp.hpl.jena.graph.GraphUtil;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 import de.uni_leipzig.informatik.swp13_sc.converter.ChessDataModelToRDFConverter;
+import de.uni_leipzig.informatik.swp13_sc.converter.ChessDataModelToRDFConverter.OutputFormats;
 import de.uni_leipzig.informatik.swp13_sc.converter.PGNToChessDataModelConverter;
 import de.uni_leipzig.informatik.swp13_sc.util.FileUtils;
 
@@ -29,13 +32,22 @@ public class VirtuosoRDFImporter
     
     public VirtuosoRDFImporter(String dbConnectionString, String username, String password)
     {
-        this.virtuosoGraph = new VirtGraph(dbConnectionString, username, password);
+        this.virtuosoGraph = new VirtGraph(
+                //"http://pcai042.informatik.uni-leipzig.de:1358/pgnconvert",
+                //"http://pcai042.informatik.uni-leipzig.de:1358/DAV",
+                dbConnectionString, username, password);
+        // or VirtModel
     }
     
     public boolean importFromZipArchive(String filename)
     {
+        // simply opening a stream
         InputStream is = FileUtils.openInputStream(filename);
-        boolean ok = this.importFromZipArchive(filename);
+        
+        // processing the stream as a zip stream
+        boolean ok = this.importFromZipArchive(is);
+        
+        // closing the stream after using it
         try
         {
             is.close();
@@ -44,25 +56,90 @@ public class VirtuosoRDFImporter
         {
             e.printStackTrace();
         }
+        
         return ok;
     }
     
     public boolean importFromZipArchive(InputStream inputStream)
     {
-        // TODO:
-        return false;
+        if (inputStream == null)
+        {
+            return false;
+        }
+        
+        // opening zip stream
+        ZipInputStream zis = null;
+        if (inputStream instanceof ZipInputStream)
+        {
+            zis = (ZipInputStream) inputStream;
+        }
+        else
+        {
+            zis = new ZipInputStream(inputStream);
+        }
+        
+        boolean ok = true;
+        
+        // processing each entry
+        ZipEntry ze = null;
+        try
+        {
+            while ((ze = zis.getNextEntry()) != null)
+            {
+                // checking for directories - not needed here
+                if (ze.isDirectory())
+                {
+                    System.out.format("Skipping Zip-Entry: %s (Directory):%n", ze.getName());
+                    continue;
+                }
+                
+                // reading a entry and working on it
+                System.out.format("Reading Zip-Entry: %s:%n", ze.getName());
+                
+                // getting the format form the extension or Turtle as default
+                OutputFormats fo = OutputFormats.TURTLE;
+                for (OutputFormats f : OutputFormats.values())
+                {
+                    if (ze.getName().endsWith(fo.getExtension()))
+                    {
+                        fo = f;
+                        break;
+                    }
+                }
+                
+                ok = ok && this.importFromRDFFileStream(zis, fo);
+                
+                zis.closeEntry();
+            }
+        }
+        catch (Exception e)
+        {
+            // ZipException
+            // IOException
+            e.printStackTrace();
+        }
+        
+        return ok;
     }
     
-    public boolean importFromRDFFileStream(InputStream inputStream)
+    public boolean importFromRDFFileStream(InputStream inputStream, OutputFormats format)
+            throws IOException
     {
-        // TODO:
+        if (inputStream == null)
+        {
+            return false;
+        }
         
-        //virtuosoGraph.read(url, type)
-        
+        // TODO:        
+        //virtuosoGraph.read(url, type)        
         VirtModel m = new VirtModel(virtuosoGraph);
         
+        // read in transaction (if possible?)
+        m.begin();
+        m.read(inputStream, null, format.getFormat());
+        m.commit();
         
-        return false;
+        return true;
     }
     
     
@@ -92,16 +169,15 @@ public class VirtuosoRDFImporter
             }
         }
         
-        // better way to add all statements?
-        StmtIterator stmtIter = m.listStatements();
-        while(stmtIter.hasNext())
-        {
-            g.add(stmtIter.nextStatement().asTriple());
-            stmtIter.remove();
-        }
+        GraphUtil.addInto(g, m.getGraph());
         
-        // or -----
-        //GraphUtil.addInto(g, m.getGraph());
+        // better way to add all statements?
+        //StmtIterator stmtIter = m.listStatements();
+        //while(stmtIter.hasNext())
+        //{
+        //    g.add(stmtIter.nextStatement().asTriple());
+        //    stmtIter.remove();
+        //}
         
         // or -----
         //VirtModel vm = new VirtModel((VirtGraph) g);
@@ -118,7 +194,9 @@ public class VirtuosoRDFImporter
     public boolean convertAndImportFromPGNFile(String inputFilename)
     {
         InputStream is = FileUtils.openInputStream(inputFilename);
+        
         boolean ok = this.convertAndImportFromPGNFile(is);
+        
         try
         {
             is.close();
@@ -127,6 +205,7 @@ public class VirtuosoRDFImporter
         {
             e.printStackTrace();
         }
+        
         return ok;
     }
     
@@ -161,18 +240,66 @@ public class VirtuosoRDFImporter
                 break;
             }
             
+            //System.out.println(cdm2rdf.getConvertedGameNames().keySet().toArray()[0]);
+            
             // put into store
             Graph dataGraph = cdm2rdf.getTripelGraph();
-            GraphUtil.addInto(this.virtuosoGraph, dataGraph);
+            System.out.println(dataGraph.size());
+            
+            // inside transaction
+            boolean transActionStarted = false;
+            try
+            {
+                // error when transaction is enabled !
+                // //this.virtuosoGraph.getTransactionHandler().transactionsSupported()
+                //this.virtuosoGraph.getTransactionHandler().begin();
+                //transActionStarted = true;
+            }
+            catch (UnsupportedOperationException e)
+            {
+                e.printStackTrace();
+            }
+            try
+            {
+                System.out.println(this.virtuosoGraph.size());
+                GraphUtil.addInto(this.virtuosoGraph, dataGraph);
+                System.out.println(this.virtuosoGraph.size());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                if (transActionStarted)
+                {
+                    this.virtuosoGraph.getTransactionHandler().abort();
+                }
+            }
+            //if (transActionStarted)
+            //{
+                try
+                {
+                    this.virtuosoGraph.getTransactionHandler().commit();
+                }
+                catch (UnsupportedOperationException e)
+                {
+                    e.printStackTrace();
+                }
+            //}
+            
             //dataGraph.clear();
             dataGraph.close();
-            
-            // access model or iterator ?
-            //com.hp.hpl.jena.graph.GraphUtil.addInto(dstGraph, srcGraph);
         }
         
         return ok;
     }
     
-    
+    public static void main(String[] args)
+    {
+        if (args.length < 4)
+        {
+            System.out.println("JavaClass/Archive: <db-connection-string> <username> <password> <file> [<ignored> ...]");
+            return;
+        }
+        VirtuosoRDFImporter vri = new VirtuosoRDFImporter(args[0], args[1], args[2]);
+        vri.convertAndImportFromPGNFile(args[3]);
+    }
 }
