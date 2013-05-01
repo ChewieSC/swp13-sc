684b6c2936563224688a8ef1f96dc3bd82c43b84
Querela <ek.92@arcor.de>
2013-05-01 23:00:33 +0200

    re-format ResourceRetriever.java ...
    remove tabs
    generate comments

--- webapp/src/de/uni_leipzig/informatik/swp13_sc/sparql/ResourceRetriever.java
+++ webapp/src/de/uni_leipzig/informatik/swp13_sc/sparql/ResourceRetriever.java
@@ -1,3 +1,7 @@
+/**
+ * ResourceRetriever.java
+ */
+
 package de.uni_leipzig.informatik.swp13_sc.sparql;
 
 import java.io.IOException;
@@ -19,100 +23,136 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import de.uni_leipzig.informatik.swp13_sc.datamodel.rdf.ChessRDFVocabulary;
 
 /**
+ * <p>
  * Servlet implementation class ResourceRetriever
+ * </p>
+ * <p>
+ * It will retrieve chess URI / IRI resources. Therefore it will query the
+ * triplestore Virtuoso with the URI / IRI as subject of each triple. The output
+ * is very basic (a simple text string).
+ * </p>
+ * 
+ * @author Erik
  */
-public class ResourceRetriever extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+public class ResourceRetriever extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ResourceRetriever() {
+    public ResourceRetriever()
+    {
         super();
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//When overriding this method, read the request data, write the response headers,
-		//get the response's writer or output stream object, and finally, write the response
-		//data. It's best to include content type and encoding. When using a PrintWriter
-		//object to return the response, set the content type before accessing the PrintWriter object. 
-		
-		
-		PrintWriter pw = response.getWriter();
-		
-		String queryString = request.getQueryString();
-		boolean extended = (queryString == null) ? false : queryString.contains("extended");
-		String resourceID = request.getPathInfo();
-		String resourceURI = ChessRDFVocabulary.getResourceURI() + resourceID.substring(1);
-		
-		String sparql = new StringBuilder()
-			.append("SELECT ")
-			.append("*")
-			.append("\n")
-			.append("WHERE {")
-			.append("\n")
-			.append("  ")
-			.append("GRAPH ?graph {")
-			.append("\n")
-			.append("    ")
-			.append("<")
-			.append(resourceURI)
-			.append(">")
-			.append(" ?p ?o .")
-			.append("\n")
-			.append((extended) ? "    " : "")
-			.append((extended) ? "?s ?p ?o ." : "")
-			.append((extended) ? "\n" : "")
-			.append("  ")
-			.append("}")
-			.append("\n")
-			.append("}")
-			.toString();
-		
-		pw.println("SPARQL-Request:");
-		pw.println(sparql);
-		pw.println();
-		
-		
-		VirtGraph virtuosoGraph = new VirtGraph("jdbc:virtuoso://pcai042.informatik.uni-leipzig.de:1357", "dba", "dba");
-		VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create (sparql, virtuosoGraph);
-		ResultSet results = vqe.execSelect();
-		
-		pw.println("Results for <" + resourceURI + ">:");
-		pw.print("[ Nr");
-		for (Object var : results.getResultVars()) {
-		    pw.print(" | ?" + var);
-		}
-		pw.println(" ] :");
-		
-		long i = 0;
-		while (results.hasNext()) {
-			QuerySolution result = results.nextSolution();
-			i ++;
-			pw.print(i);
-			
-			for (Object var : results.getResultVars()) {
-			    try {
-			        RDFNode node = result.get((String) var);
-			        pw.print(" | " + node);
-			    } catch (Exception e) {
-			    	e.printStackTrace();
-			    	
-			    	pw.close();
-			    	return;
-			    }
-			}
-			
-		    pw.println();
-		}
-		
-		pw.println();
-		pw.println("Number of results: " + i);
-		
-		pw.flush();
-		pw.close();
-	}
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // When overriding this method, read the request data, write the
+        // response headers, get the response's writer or output stream object,
+        // and finally, write the response data. It's best to include content
+        // type and encoding. When using a PrintWriter object to return the
+        // response, set the content type before accessing the PrintWriter
+        // object.
+        
+        // for writing the response (very simple)
+        PrintWriter pw = response.getWriter();
+        
+        // get the GET parameters / request variables
+        // and contruct the chess resource URI / IRI
+        String queryString = request.getQueryString();
+        boolean extended = (queryString == null) ? false : queryString.contains("extended");
+        String resourceID = request.getPathInfo();
+        String resourceURI = ChessRDFVocabulary.getResourceURI() + resourceID.substring(1);
+        
+        // construct the SPARQL query
+        String sparql = new StringBuilder()
+                .append("SELECT ")
+                .append("*")
+                .append("\n")
+                .append("WHERE {")
+                .append("\n")
+                .append("  ")
+                .append("GRAPH ?graph {")
+                .append("\n")
+                .append("    ")
+                .append("<")
+                .append(resourceURI)
+                .append(">")
+                .append(" ?p ?o .")
+                .append("\n")
+                .append((extended) ? "    " : "")
+                .append((extended) ? "?s ?p ?o ." : "")
+                .append((extended) ? "\n" : "")
+                .append("  ")
+                .append("}")
+                .append("\n")
+                .append("}")
+                .toString();
+        
+        // print the complete query
+        pw.println("SPARQL-Request:");
+        pw.println(sparql);
+        pw.println();
+        
+        // query Virtuoso
+        VirtGraph virtuosoGraph = new VirtGraph("jdbc:virtuoso://pcai042.informatik.uni-leipzig.de:1357", "dba", "dba");
+        VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(sparql, virtuosoGraph);
+        ResultSet results = vqe.execSelect();
+        
+        // header of result resource section
+        pw.println("Results for <" + resourceURI + ">:");
+        pw.print("[ Nr");
+        for (Object var : results.getResultVars())
+        {
+            pw.print(" | ?" + var);
+        }
+        pw.println(" ] :");
+        
+        // print all resources
+        long i = 0;
+        while (results.hasNext())
+        {
+            QuerySolution result = results.nextSolution();
+            
+            // the current number
+            i++;
+            pw.print(i);
+            
+            // write the value for each variable in the query
+            for (Object var : results.getResultVars())
+            {
+                try
+                {
+                    RDFNode node = result.get((String) var);
+                    pw.print(" | " + node);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    
+                    // abort when error
+                    // don't resume because there may be more than one and
+                    // we don't want to have lots of meaningless output ...
+                    pw.close();
+                    return;
+                }
+            }
+            
+            pw.println();
+        }
+        
+        // number of results
+        pw.println();
+        pw.println("Number of results: " + i);
+        
+        // flush and close output
+        pw.flush();
+        pw.close();
+    }
 }
