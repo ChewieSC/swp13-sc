41778450b79a0c0e5ec45feea3be29982eaa183a
Querela <ek.92@arcor.de>
2013-06-03 18:09:20 +0200

    Better ECO-CG-Linker.
    Requires command line args.
    Separated class storage data methods.

--- linking/src/de/uni_leipzig/informatik/swp13_sc/linking/ECOLinker.java
+++ linking/src/de/uni_leipzig/informatik/swp13_sc/linking/ECOLinker.java
@@ -20,7 +20,6 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
 
 import de.uni_leipzig.informatik.swp13_sc.converter.ChessDataModelToRDFConverter.OutputFormats;
 import de.uni_leipzig.informatik.swp13_sc.datamodel.ChessOpening;
@@ -41,11 +40,13 @@ public class ECOLinker
     private boolean hasMappings;
     protected ChessOpeningDataRetriever codr;
     
+    public final static int QUERY_TIMEOUT = 90;
+    
     // ------------------------------------------------------------------------
     
     protected final static String SPARQL_ECOS_VAR = "?ECO_URI";
     protected final static String SPARQL_ECOS_GRAPH = "eco";
-    protected final static String SPARQL_ECOS_FROM = "FROM <" + SPARQL_ECOS_GRAPH + ">\n";
+    protected final static String SPARQL_ECOS_FROM = ""; //"FROM <" + SPARQL_ECOS_GRAPH + ">\n";
     protected final static String SPARQL_ECOS = "SELECT DISTINCT "
             + SPARQL_ECOS_VAR + "\n" + SPARQL_ECOS_FROM + "WHERE\n{\n  "
             + SPARQL_ECOS_VAR + " a cont:ChessOpening .\n}\nORDER BY " + SPARQL_ECOS_VAR;
@@ -57,13 +58,31 @@ public class ECOLinker
     
     // ------------------------------------------------------------------------
     
+    /**
+     * Creates a new ECOLinker object. Requires a VirtGraph to query a Virtuoso
+     * triple store. Will set its query time out to 90 seconds to allow
+     * efficient work.
+     * 
+     * @param   virtuosoGraph   Graph (connection) to triple store
+     */
     public ECOLinker(VirtGraph virtuosoGraph)
     {
+        this(virtuosoGraph, QUERY_TIMEOUT);
+    }
+    
+    /**
+     * Creates a new ECOLinker object. Takes a new VirtGraph connection to a
+     * Virtuoso triple store and sets it to the specified timeout. Creates a
+     * new ChessOpeningDataRetriever with the same connection.
+     * 
+     * @param   virtuosoGraph   connection to Virtuoso triple store
+     * @param   timeout     timeout in seconds
+     */
+    public ECOLinker(VirtGraph virtuosoGraph, int timeout)
+    {
         this.virtuosoGraph = virtuosoGraph;
-        //this.virtuosoGraph.setQueryTimeout(300); // in sec
-        this.virtuosoGraph.setQueryTimeout(90);
+        this.virtuosoGraph.setQueryTimeout(timeout); // sec.
         
-        this.ecoGameMapping = new HashMap<String, List<String>>();
         this.hasMappings = false;
         
         try
@@ -78,6 +97,11 @@ public class ECOLinker
     
     // ------------------------------------------------------------------------
     
+    /**
+     * Queries the triple store the retrieve all the opening (ECO) URIs.
+     * 
+     * @return  List<String> with chess opening (ECO) URIs or null on error
+     */
     public List<String> getOpeningURIs()
     {
         List<String> list = new ArrayList<String>();
@@ -109,9 +133,16 @@ public class ECOLinker
         return list;
     }
     
+    /**
+     * Will query the triple store for games with equal opening moves like
+     * those stored in the ChessOpening object.
+     * 
+     * @param   co  ChessOpening object (contains the opening moves)
+     * @return  List<String> with mapped games for those opening moves
+     */
     public List<String> getGamesForOpenings(ChessOpening co)
     {
-        if (co == null)
+        if ((co == null) || (co.getMoves() == null) || (co.getMoves().isEmpty()))
         {
             return new ArrayList<String>();
         }
@@ -213,30 +244,39 @@ public class ECOLinker
         return list;
     }
     
-    public Map<String, List<String>> getMappings()
-    {
-        if ((this.hasMappings) && (this.ecoGameMapping != null)
-                && (this.ecoGameMapping.size() > 0))
-        {
-            return this.ecoGameMapping;
-        }
-        
+    /**
+     * For the given URIs it will query the VirtGraph to get chess openings
+     * (ECOs). It will then take the moves to get chess games with equal
+     * openings and returns them for the opening URI.
+     * 
+     * @param   uris    URIs of chess openings
+     * @return  Map with URI of chess opening (ECO) and mapped games (URIs)
+     */
+    public Map<String, List<String>> getMappings(List<String> uris)
+    {   
         if (this.codr == null)
         {
-            return null;
+            System.err.println("Has no valid ChessOpeningDataRetriever!");
+            return new HashMap<String, List<String>>();
         }
         
-        // get URIs of openings / ECOs
-        List<String> uris = this.getOpeningURIs();
         if ((uris == null) || (uris.size() == 0))
         {
-            return null;
+            System.err.println("No URIs to get ChessOntologies (ECOs) from!");
+            return new HashMap<String, List<String>>();
         }
+        
+        Map<String, List<String>> mappings = new HashMap<String, List<String>>();
         
         // get Openings / ECOs objects
         for (String uri : uris)
         {
             ChessOpening co = this.codr.getOpening(uri);
+            if (co == null)
+            {
+                System.err.println("<" + uri + "> is NULL ?");
+                continue;
+            }
             
             long start = System.currentTimeMillis();
             List<String> games = getGamesForOpenings(co);
@@ -246,42 +286,98 @@ public class ECOLinker
                     Float.toString(((System.currentTimeMillis() - start) / 1000.f)),
                     games.size(), co);
             
-            this.ecoGameMapping.put(uri, games);
+            mappings.put(uri, games);
         }
+        
+        return mappings;
+    }
+    
+    // ------------------------------------------------------------------------   
+    
+    /**
+     * Will retrieve all chess openings (ECOs) stored in the triple store and
+     * tries to get all chess games to which the opening moves can be mapped.
+     * It will store the results in the class.
+     * 
+     * @return  cached map with all the mapped chess games and their openings
+     */
+    public Map<String, List<String>> getMappings()
+    {
+        if ((this.hasMappings) && (this.ecoGameMapping != null)
+                && (this.ecoGameMapping.size() > 0))
+        {
+            return this.ecoGameMapping;
+        }
+        
+        this.ecoGameMapping = new HashMap<String, List<String>>();
+        this.ecoGameMapping = this.getMappings(this.getOpeningURIs());
         
         this.hasMappings = true;
         
+        // get URIs of openings / ECOs
         return this.ecoGameMapping;
     }
     
-    public Model createMappingModel()
+    // ------------------------------------------------------------------------
+    
+    /**
+     * Creates a default Model with prefixes for chess rdf data.
+     * 
+     * @return  a new Model (Jena)
+     */
+    protected Model createDefaultModel()
     {
-        this.getMappings();
-        
         Model m = ModelFactory.createDefaultModel();
+        m.setNsPrefix(ChessRDFVocabulary.getOntologyPrefixName(),
+                ChessRDFVocabulary.getURI());
+        m.setNsPrefix(ChessRDFVocabulary.getResourcePrefixName(),
+                ChessRDFVocabulary.getResourceURI());
+        return m;
+    }
+    
+    /**
+     * Creates a new Mapping-Model and fills it with the given mappings.
+     * 
+     * @param   mappings    Map with ECO URI and mapped chess game URIs
+     * @return  a new Model (Jena) with mappings (it may be empty if the given
+     *          mappings are empty!)
+     */
+    public Model createMappingModel(Map<String, List<String>> mappings)
+    {
+        Model m = createDefaultModel();
+        if (mappings == null)
+        {
+            System.err.println("No mappings for model!");
+            return m;
+        }
         
         for (String eco : this.ecoGameMapping.keySet())
         {
-            Resource r_eco = m.createResource(eco, ChessRDFVocabulary.ChessOpening);
+            Resource r_eco = m.createResource(eco);
             
             for (String game : this.ecoGameMapping.get(eco))
             {
-                Resource r_game = m.createResource(game, ChessRDFVocabulary.ChessGame);
+                Resource r_game = m.createResource(game);
                 
-                Statement stmt = m.createStatement(r_game, ChessRDFVocabulary.eco, r_eco);
-                System.out.println(stmt);
-                m.add(stmt);
-                
-                //m.add(r_game, ChessRDFVocabulary.eco, r_eco);
+                m.add(r_game, ChessRDFVocabulary.eco, r_eco);
             }
         }
         
         return m;
     }
     
+    // ------------------------------------------------------------------------
+    
+    /**
+     * Creates a new Mapping-Model, fills it with queried mappings and writes
+     * it to the specified file.
+     * 
+     * @param   file    File to write into (GZIP-Stream)
+     * @return  true if successful
+     */
     public boolean writeMappingModel(String file)
     {
-        Model m = this.createMappingModel();
+        Model m = this.createMappingModel(this.getMappings());
         
         try
         {
@@ -308,13 +404,32 @@ public class ECOLinker
     // ------------------------------------------------------------------------
     
     /**
-     * @param args
+     * Main method. Start from command line.
+     * Will create a default mapping and stores it in the file.
+     * 
+     * @param   args    <ol>
+     *                      <li> graph to query</li>
+     *                      <li> link to database</li>
+     *                      <li> username</li>
+     *                      <li> password</li>
+     *                      <li> [outputfile  (default: Mapping_ECO_GAME.ttl)]</li>
+     *                  </ol>
      */
     public static void main(String[] args)
     {
-        ECOLinker ecol = new ECOLinker(new VirtGraph("millionbase",
-                "jdbc:virtuoso://pcai042.informatik.uni-leipzig.de:1357",
-                "dba", "dba"));
-        ecol.writeMappingModel("Mapping_ECO_GAME.ttl");
+        if ((args.length < 4) || (args.length > 6))
+        {
+            System.exit(1);
+        }
+        
+        String graph = args[0];
+        String link = args[1];
+        String user = args[2];
+        String pass = args[3];
+        String file = (args.length == 5) ? args[4] : "Mapping_ECO_GAME.ttl";
+        
+        ECOLinker ecol = new ECOLinker(new VirtGraph(graph, "jdbc:virtuoso://"
+                + link, user, pass));
+        ecol.writeMappingModel(file);
     }
 }
